2d9922b79f94947759f5d82c8ba1e8aae314cc27
Vishnu12222344 <119558844+Vishnu12222344@users.noreply.github.com>
2025-12-24 22:18:47 +0530

    Initial commit

---
+++ HCL-Training-master/src/javapractice/Inheritance.java
@@ -0,0 +1,35 @@
+package javapractice;
+
+class Calc{
+
+    public int add(int a,int b){
+        return a+b;
+    }
+    public int sub(int a,int b){
+        return a-b;
+    }
+
+}
+class AdvCalc extends Calc{
+    public int mul(int a,int b){
+        return a*b;
+    }
+    public int div(int a, int b){
+        return a/b;
+    }
+}
+
+public class Inheritance {
+    public static void main(String[] args){
+        AdvCalc obj=new AdvCalc();    // if we create object or only Calc() then we cannot move with mul and div.
+        int r1= obj.add(3,4);           // so we need to create for AdvCalc().
+        int r2= obj.sub(3,4);
+        int r3= obj.mul(3,4);
+        int r4= obj.div(3,4);
+        System.out.println(r1+" "+r2+" "+r3+" "+r4);
+
+    }
+}
+
+/* Multiple inheritance is not possible in java because if we are having same methods in parent and child then the obj do
+not have idea which method it should call. so to overcome with the problem java removed Multiple inheritance.*/
